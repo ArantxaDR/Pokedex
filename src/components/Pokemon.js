0b789b1cf935bf9e50eef8651b437b1acbdeea31
ArantxaDR <arantxaderuiz@gmail.com>
2020-11-12 12:54:47 +0100

    add types array

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,14 +1,18 @@
 import React from "react";
 
 const Pokemon = (props) => {
+  console.log(props.poke.types);
+  const pokeType = props.poke.types.map((type, index) => {
+    return <li key={index}>{type}</li>;
+  });
   return (
-    <article clasName="cards">
+    <article className="cards">
       <img src={props.poke.url} alt={props.poke.name} />
       <div>
         <h2 className="name">{props.poke.name}</h2>
       </div>
       <div>
-        <h3 className="type">{props.poke.types}</h3>
+        <ul className="types">{pokeType}</ul>
       </div>
     </article>
   );
