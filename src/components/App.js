1c60c06f9330660f418289893cc2bdfbb7901185
ArantxaDR <arantxaderuiz@gmail.com>
2020-11-12 13:19:42 +0100

    create header and footer

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,18 @@ import React from "react";
 import PokeJson from "../data/PokeJson.json";
 import PokeList from "./PokeList.js";
 import "../stylesheets/App.css";
+import Header from "./Header.js";
+import Footer from "./Footer.js";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Pokedex</h1>
-        </header>
+        <Header />
         <main className="pokeMain">
           <PokeList Pokemon={PokeJson} />
         </main>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="App-footer">
+        <small>Pokedex &copy; Arantxa Delgado Ruiz</small>
+      </footer>
+    );
+  }
+}
+export default Footer;
