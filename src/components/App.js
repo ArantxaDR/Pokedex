70d01bfe22c9b49ef1142ce8879bd9435a25eb9b
ArantxaDR <arantxaderuiz@gmail.com>
2021-02-08 18:17:53 +0100

    api fetch

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,19 @@ import PokeFilters from "./PokeFilters.js";
 import "../stylesheets/App.scss";
 import Header from "./Header.js";
 import Footer from "./Footer.js";
+
 import PokeDetails from "./PokeDetails.js";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import GetDataFromApi from "../services/GetDataFromApi.js";
 
 const App = () => {
+  const [pokemons, setPokemons] = useState([]);
   useEffect(() => {
     GetDataFromApi().then((data) => {
-      console.log(data);
+      setPokemons(data);
     });
-  });
-
+  }, []);
+  console.log(pokemons);
   return (
     <>
       <div className="App">
--- src/services/GetDataFromApi.js
+++ src/services/GetDataFromApi.js
@@ -1,11 +1,14 @@
+const arr = [];
 const apiCall = () => {
-  return fetch(`https://pokeapi.co/api/v2`)
+  return fetch("https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0")
     .then((response) => response.json())
     .then((data) => {
-      return data.results.map((pokemon) => {
-        console.log(pokemon);
-        return {};
+      return data.results.map((item) => {
+        fetch(item.url)
+          .then((response) => response.json())
+           .then((allpokemon) =>console.log(allpokemon); //arr.push(allpokemon));
       });
+      
     });
 };
 
